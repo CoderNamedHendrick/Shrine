d184c4f7cfdb8691f0f28413fcbb81f697e2fe46
Rookiecoder17 <52936414+Rookiecoder17@users.noreply.github.com>
2020-12-21 13:01:16 +0000

    Added functionality to the add button

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -1,11 +1,19 @@
 package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -17,11 +25,38 @@ public class LoginFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        Button nextButton = view.findViewById(R.id.next_button);
 
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isPasswordValid(passwordEditText.getText()))
+                    passwordTextInput.setError(getString(R.string.shr_error_password)); // error message if the user input is in the wrong format.
+                else {
+                    passwordTextInput.setError(null); // Clear the error
+                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false);
+                }
+            }
+        });
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (isPasswordValid(passwordEditText.getText()))
+                    passwordTextInput.setError(null); // Clear the error
+                return false;
+            }
+        });
         // Snippet from "Navigate to the next Fragment" section goes here.
-
         return view;
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private boolean isPasswordValid(@Nullable Editable text){
+        return text != null && text.length() >= 8;
+    }
 }
